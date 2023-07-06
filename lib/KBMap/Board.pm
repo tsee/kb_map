db1abfe7722fb2acd0fbba5196e628d6adcec1e3
Steffen Mueller <smueller@cpan.org>
2023-07-06 23:56:58 +0100

    Add absurd HTML canvas/JS frontend

--- lib/KBMap/Board.pm
+++ lib/KBMap/Board.pm
@@ -3,35 +3,39 @@ package KBMap::Board;
 use warnings;
 use feature 'signatures';
 use Exporter 'import';
+use JSON;
 
-our %FieldTypes;
+our %TileTypes;
+our %ValidTileTypes;
 BEGIN {
-  %FieldTypes = (
-    F_NONE => 0, # still undefined tile
-    F_CANYON => 1,
-    F_DESERT => 2,
-    F_FLOWERS => 3,
-    F_FOREST => 4,
-    F_GRASS => 5,
-    F_WATER => 6,
-    F_MOUNTAIN => 7,
-    F_CASTLE => 8,
+  %TileTypes = (
+    F_NONE => "none", # still undefined tile
+    F_CANYON => "canyon",
+    F_DESERT => "desert",
+    F_FLOWERS => "flowers",
+    F_FOREST => "forest",
+    F_GRASS => "grass",
+    F_WATER => "water",
+    F_MOUNTAIN => "mountain",
+    F_CASTLE => "castle",
   );
+
+  %ValidTileTypes = map {$_ => 1} values(%TileTypes);
 }
 use constant {
-  %FieldTypes,
+  %TileTypes,
   WIDTH => 10,
   HEIGHT => 10,
 };
 
 our @EXPORT_OK = (
-  keys(%FieldTypes),
+  keys(%TileTypes),
 );
 our @EXPORT = @EXPORT_OK;
 
 # a kb board is represented as an array of 100 tiles, arranged in a 10x10 row
 # major matrix (first row one, then row two, ...) for display and rendering.
-sub new ($class, %args) {
+sub new($class, %args) {
   my $self = bless {
     tiles => [],
   } => $class;
@@ -41,15 +45,19 @@ sub new ($class, %args) {
   return $self;
 }
 
-sub reset_board ($self) {
+sub reset_board($self) {
   @{ $self->{tiles} } = ( (F_NONE) x (WIDTH*HEIGHT) );
 }
 
-sub tiles ($self) {
+sub tiles($self) {
   return $self->{tiles};
 }
 
-sub render_ascii ($self) {
+sub set_tile($self, $col, $row, $type) {
+  $self->{tiles}[$row * WIDTH + $col] = $type;
+}
+
+sub render_ascii($self) {
   my @lines;
   foreach my $row (0..HEIGHT-1) {
     my $i = $row * WIDTH;
@@ -58,6 +66,23 @@ sub render_ascii ($self) {
   }
 
   return \@lines;
+}
+
+sub render_json($self) {
+  return encode_json({ tiles => $self->{tiles} });
+}
+
+sub is_valid_tile_type($self, $type) {
+  return if not defined $type;
+  return $ValidTileTypes{$type};
+}
+
+sub all_valid_tile_types($self, @types) {
+  foreach my $type (@types) {
+    return if not defined $type;
+    return if not $ValidTileTypes{$type};
+  }
+  return 1;
 }
 
 1;
--- lib/KBMap/Server.pm
+++ lib/KBMap/Server.pm
@@ -12,15 +12,23 @@ use KBMap::Board;
 use File::Spec;
 use bytes ();
 
+use constant {
+  CANVAS_FILE => 'canvas.html',
+};
+
 # Static web actions dispatch state
 my %dispatch = (
-  '/hello' => \&_resp_hello,
+  '/'          => \&_resp_canvas,
+  '/board'     => \&_resp_board,
+  '/board_set' => \&_resp_board_set,
+  '/hello'     => \&_resp_hello,
 );
 
 sub new ($class, %args) {
   my $self = $class->SUPER::new(%args);
   $self->{_app_state} = {
     img_path => $args{img_path} // 'img',
+    static_path => $args{static_path} // 'static',
     board => KBMap::Board->new,
   };
   return $self;
@@ -44,7 +52,6 @@ sub handle_request ($self, $cgi) {
   my $handler = $dispatch{$path};
 
   if (ref($handler) eq "CODE") {
-    $self->_http_ok;
     $self->$handler($cgi);
   } else {
     $self->_http_not_found;
@@ -55,44 +62,101 @@ sub handle_request ($self, $cgi) {
   }
 }
 
+sub _resp_canvas($self, $cgi) {
+  return if !ref $cgi;
+
+  my $local_path = File::Spec->catfile($self->state->{static_path}, CANVAS_FILE);
+
+  return $self->_serve_static(
+    $cgi,
+    $local_path,
+    -type => 'text/html',
+  );
+}
+
+sub _resp_board($self, $cgi) {
+  return if !ref $cgi;
+
+  print "HTTP/1.1 200\r\n";
+  print $cgi->header(
+    -status => '200 OK',
+    -type => 'text/json',
+  );
+  print $self->{_app_state}{board}->render_json;
+}
+
+sub _resp_board_set($self, $cgi) {
+  return if !ref $cgi;
+
+  my $row = $cgi->param('row');
+  my $col = $cgi->param('col');
+  my $type = $cgi->param('type');
+
+  if (not $self->{_app_state}{board}->is_valid_tile_type($type)) {
+    $self->_http_bad_request("Invalid tile type '$type'");
+    return;
+  }
+
+  $self->{_app_state}{board}->set_tile($col, $row, $type);
+  print "HTTP/1.1 200\r\n";
+  print $cgi->header(
+    -status => '200 OK',
+  );
+}
+
+
 # handler to serve an image from the configured image path
-sub _serve_image ($self, $cgi, $path) {
+sub _serve_image($self, $cgi, $path) {
   return if !ref $cgi;
 
   # validate simple path structure (KISS will also keep it secure)
-  if (not $path =~ qr{^/img/(\w+\.png)$}i) {
+  if (not $path =~ qr{^/img/([\w+_-]+\.png)$}i) {
     $self->_debug("Invalid image path '$path'");
     $self->_http_not_found;
     return;
   };
 
-  # find the file and read it
   my $filename = $1;
-  my $imgpath = File::Spec->catfile($self->state->{img_path}, $filename);
-  if (not -f $imgpath) {
-    $self->_debug("Could not find file '$imgpath' for web path '$path'");
+  my $local_path = File::Spec->catfile($self->state->{img_path}, $filename);
+
+  return $self->_serve_static(
+    $cgi,
+    $local_path,
+    -type => 'image/png',
+  );
+}
+
+sub _serve_static($self, $cgi, $local_path, %headers) {
+  # find the file and read it
+  if (not -f $local_path) {
+    $self->_debug("Could not find file '$local_path' for web path '".$cgi->path_info()."'");
     $self->_http_not_found;
     return;
   }
 
-  my $content = _slurp_file($imgpath);
+  my $content = _slurp_file($local_path);
   if (not defined $content) {
-    $self->_debug("Could not read file '$imgpath' for web path '$path'");
+    $self->_debug("Could not read file '$local_path' for web path '".$cgi->path_info()."'");
     $self->_http_not_found;
     return;
   }
 
   # found it, serve it
-  $self->_http_ok;
+  %headers = (
+      %headers,
+      -content_length => bytes::length($content),
+      -status => 200,
+  );
+  print "HTTP/1.1 200\r\n";
+  print $cgi->header(%headers);
   binmode STDOUT;
-  print $cgi->header(
-          type => 'image/png',
-          content_length => bytes::length($content),
-          expires => '+1h',
-        ),
-        $content;
+  print $content;
+  return 1;
 }
 
+# Would be better to read/write chunked, but these are small files, I'm lazy, and
+# to be perfectly frank, I bet there's full buffering somewhere in the simple web
+# server code anyway.
 sub _slurp_file ($path) {
   open my $fh, "<", $path or return undef;
   binmode $fh;
@@ -102,12 +166,14 @@ sub _slurp_file ($path) {
   return $content;
 }
 
+# debug action
 sub _resp_hello ($self, $cgi) {
 
   $self->_debug($_) for @{ $self->state->{board}->render_ascii };
 
   my $who = $cgi->param('name');
 
+  print "HTTP/1.1 200\r\n";
   print $cgi->header,
         $cgi->start_html("Hello"),
         $cgi->h1("Hello $who!"),
@@ -125,12 +191,17 @@ sub _debug ($server, @msgs) {
   }
 }
 
-sub _http_not_found ($self) {
-  print "http/1.0 404 not found\r\n";
+sub _http_not_found($self) {
+  print "HTTP/1.1 404 Not found\r\n";
 }
 
-sub _http_ok ($self) {
-  print "HTTP/1.0 404 Not found\r\n";
+sub _http_bad_request($self, $msg) {
+  print "HTTP/1.1 400 $msg\r\n";
+}
+
+sub _http_ok($self) {
+  print "HTTP/1.1 200 OK\r\n";
 }
 
 1;
+
