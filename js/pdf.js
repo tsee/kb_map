1648d2b67340a4d268b5238848d2455ff417e96c
Steffen Mueller <smueller@cpan.org>
2023-07-21 14:00:50 +0100

    Less awful pdf map layout code

--- js/pdf.js
+++ js/pdf.js
@@ -9,7 +9,7 @@ import {
 
 import { Point, OffsetCoord, Layout } from '../vendor/hexagons/lib-module.js';
 
-export function generate_pdf(cfg, map, layout) {
+export function generate_pdf(cfg, map) {
   const { jsPDF } = window.jspdf;
   const doc = new jsPDF({
     orientation: 'portrait',
@@ -17,21 +17,33 @@ export function generate_pdf(cfg, map, layout) {
     format: 'a4',
   });
 
-  let draw_x_offset_cm = 0.5;
-  let draw_y_offset_cm = 1.0;
+  const tile_side_in_cm = 1.4;
+  const tile_width_in_cm = Math.sqrt(3) * tile_side_in_cm;
+  const tile_height_in_cm = 2 * tile_side_in_cm; // for pointy side up
+
+  const print_layout = new Layout(
+    Layout.pointy,
+    // tile sizes in both dimensions in cm.
+    new Point(tile_width_in_cm / Math.sqrt(3), tile_height_in_cm / 2),
+    // origin coords
+    new Point(0, 0)
+  );
+
+  let draw_x_offset_cm = 1.5;
+  let draw_y_offset_cm = 1.5;
 
   // local helper function to reduce copy/paste.
   // Yes, 'tis not the height of programming.
   let draw_hex_helper = function(v) {
     let tt = tile_types[v.type];
-    let p = layout.hexToPixel(v.hex);
-    const px_to_cm = 96/72 / (72/2.54);
+    let p = print_layout.hexToPixel(v.hex);
+
     doc.addImage(
         tt.img, 'PNG',
-        p.x * px_to_cm + draw_x_offset_cm,
-        p.y * px_to_cm + draw_y_offset_cm,
-        cfg.tile_width * px_to_cm,
-        cfg.tile_height * px_to_cm,
+        p.x + draw_x_offset_cm,
+        p.y + draw_y_offset_cm,
+        tile_width_in_cm,
+        tile_height_in_cm,
         v.type
     );
   };
@@ -46,7 +58,7 @@ export function generate_pdf(cfg, map, layout) {
   });
 
   doc.addPage();
-  draw_x_offset_cm = -17;
+  draw_x_offset_cm = -10;
   map.iterate(function(v) {
       if (OffsetCoord.roffsetFromCube(1, v.hex).col < 6)
         return;
